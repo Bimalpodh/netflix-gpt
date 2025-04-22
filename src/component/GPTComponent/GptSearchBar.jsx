411f2137ad7098c92ca58def33c3e4ca61e9b4a7
Bimal_Babu <bimalpodh08@gmail.com>
2025-04-22 22:58:44 +0530

    Update GptSearchBar.jsx

--- src/component/GPTComponent/GptSearchBar.jsx
+++ src/component/GPTComponent/GptSearchBar.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import geminiModel from "../../utils/openAi/"
 import { API_OPTION } from '../../utils/constant'
 import { addGeminiMoviesResults } from '../../reduxStore/gptslice'
-import gptslice from '../../reduxStore/gptslice';
 
 
 const GptSearchBar = () => {
